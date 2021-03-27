037c4a8614294fc8434517a116321a5cc2583b12
Hari Sharma <harishar@umich.edu>
2021-03-26 21:22:23 -0700

    Added code to get table. Working on button.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+data.forEach(function(sighting){
+    console.log(sighting);
+    var row = tbody.append("tr");
+
+    Object.entries(sighting).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+var button = d3.select("filter-btn");
+button.on("click", function(){
+
+// Implement code here ***
+})

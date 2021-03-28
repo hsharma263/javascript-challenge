00d0d18c49be480f49b0ee39544e2a3226e60f37
Hari Sharma <harishar@umich.edu>
2021-03-28 10:10:25 -0700

    Implemented code for filter button.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,26 +1,39 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-
 var tbody = d3.select("tbody");
 
 console.log(tableData);
 data.forEach(function(sighting){
-    console.log(sighting);
+    // console.log(sighting);
     var row = tbody.append("tr");
 
     Object.entries(sighting).forEach(function([key, value]){
-        console.log(key, value);
+        // console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
 });
 
 
-var button = d3.select("filter-btn");
+var button = d3.select("#filter-btn");
 button.on("click", function(){
 
-// Implement code here ***
-})
+    var inputDate = d3.select("#datetime");
+    var inputInfo = inputDate.property("value");
+    console.log(inputDate);
+
+    var filtered_data = tableData.filter(event => event.datetime === inputInfo);
+    tbody.html("");
+    console.log(filtered_data);
+
+    filtered_data.forEach(function(input) {
+        console.log(input);
+        var row = tbody.append("tr");
+        Object.entries(input).forEach(function([key, value]){
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
